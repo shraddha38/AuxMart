cda5afc76123e9b35557e86c586fce6349f43bac
Sraddha <8770473516sraddha@gmail.com>
2021-07-27 23:48:57 +0530

    added

---
+++ src/datasource/Vegetable.js
@@ -0,0 +1,44 @@
+export const VegetableFruits = [
+    {
+      id: 1,
+      title: "Capsicum",
+      img: "src/images/Capsicum.jpg",
+      price: 50,
+      info: "Fresh from the farms",
+      inCart: false,
+      count: 0,
+      total: 0,
+    },
+    {
+        id: 2,
+        title: "Cabbage",
+        img: "src/images/Cabbage.jpg",
+        price: 20,
+        info: "Fresh from the farms",
+        inCart: false,
+        count: 0,
+        total: 0,
+      },
+      {
+        id: 3,
+        title: "Carrot",
+        img: "src/images/Carrot.jpg",
+        price: 30,
+        info: "Fresh from the farms",
+        inCart: false,
+        count: 0,
+        total: 0,
+      },
+      {
+        id: 4,
+        title: "Brinjal",
+        img: "src/images/Brinjal.jpg",
+        price: 40,
+        info: "Fresh from the farms",
+        inCart: false,
+        count: 0,
+        total: 0,
+      },
+
+
+]
